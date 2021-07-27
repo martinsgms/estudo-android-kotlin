0ba88ba421d245daf810714547889599147015e2
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-07-26 22:46:22 -0300

    cria view para produto

--- app/src/main/java/br/com/martinsgms/lojapp/MainActivity.kt
+++ app/src/main/java/br/com/martinsgms/lojapp/MainActivity.kt
@@ -2,13 +2,13 @@ package br.com.martinsgms.lojapp
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem-vindo ao Lojapp", Toast.LENGTH_SHORT).show()
+
+        setContentView(R.layout.activity_main)
 
     }
 }
