fbc6177b37d393d15eb7723715a30d55a82a50df
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-07-27 21:13:42 -0300

    realiza bind da view com o layout e introduz recyclerView

--- app/src/main/java/br/com/martinsgms/lojapp/MainActivity.kt
+++ app/src/main/java/br/com/martinsgms/lojapp/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.martinsgms.lojapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -9,6 +10,15 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val titulo = findViewById<TextView>(R.id.titulo)
+        titulo.text = "Celular"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Bom celular, tela OLED"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "R$ 1.299,00"
 
     }
 }
